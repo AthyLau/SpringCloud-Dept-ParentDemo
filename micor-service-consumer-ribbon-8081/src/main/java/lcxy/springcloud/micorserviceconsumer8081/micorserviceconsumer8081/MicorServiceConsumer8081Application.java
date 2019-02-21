d0bbf5d67b6d06b9455ddfb4e1b30283219a6dbd
liubing <1015141113@qq.com>
2019-02-21 15:15:13 +0800

    不同包非子包的自定义Ribbon配置负载均衡类加configuration注解

--- micor-service-consumer-ribbon-8081/src/main/java/lcxy/springcloud/micorserviceconsumer8081/micorserviceconsumer8081/MicorServiceConsumer8081Application.java
+++ micor-service-consumer-ribbon-8081/src/main/java/lcxy/springcloud/micorserviceconsumer8081/micorserviceconsumer8081/MicorServiceConsumer8081Application.java
@@ -1,7 +1,9 @@
 package lcxy.springcloud.micorserviceconsumer8081.micorserviceconsumer8081;
 
-import lcxy.springcloud.micorserviceconsumer8081.micorserviceconsumer8081.config.DeptLoadBalancedConfig;
-import lcxy.springcloud.micorserviceconsumer8081.micorserviceconsumer8081.config.EmployeeLoadBalanceConfig;
+//import lcxy.springcloud.micorserviceconsumer8081.micorserviceconsumer8081.config.DeptLoadBalancedConfig;
+//import lcxy.springcloud.micorserviceconsumer8081.micorserviceconsumer8081.config.EmployeeLoadBalanceConfig;
+import lcxy.springcloud.micorserviceconsumer8081.myiruls.DeptLoadBalancedConfig;
+import lcxy.springcloud.micorserviceconsumer8081.myiruls.EmployeeLoadBalanceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,6 +12,11 @@ import org.springframework.cloud.netflix.ribbon.RibbonClients;
 
 //此ribbon客户端访问DEPT-PROVIDER服务时调用LoadBalancedConfig配置类中配置的负载均衡策略
 //@RibbonClient(name = "DEPT-PROVIDER",configuration = DeptLoadBalancedConfig.class)
+//自定义配置类+了@Configuration注解之后那么这个配置类不能放在@ComponentScan所扫描的当前包以及子包下
+//@RibbonClients(value = {
+//		@RibbonClient(name = "DEPT-PROVIDER",configuration = DeptLoadBalancedConfig.class),
+//		@RibbonClient(name = "EMPLOYEE-PROVIDER",configuration = EmployeeLoadBalanceConfig.class)
+//})
 @RibbonClients(value = {
 		@RibbonClient(name = "DEPT-PROVIDER",configuration = DeptLoadBalancedConfig.class),
 		@RibbonClient(name = "EMPLOYEE-PROVIDER",configuration = EmployeeLoadBalanceConfig.class)
